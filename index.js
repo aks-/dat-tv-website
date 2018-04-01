9811591f36fec71ea37f84792d6c8be332d213bd
Ashok <coderatlabs@gmail.com>
2018-04-01 12:01:18 +0530

    add script to start video

---
+++ index.js
@@ -0,0 +1,32 @@
+var btn = document.querySelector('button')
+var vid = document.querySelector('video')
+var datlink = null
+
+function isDatUrl (url) {
+  var isdaturl = false
+  try {
+    isdaturl = (new URL(url).protocol) === 'dat:'
+  } catch (_) {}
+
+  return isdaturl
+}
+
+btn.addEventListener('click', function () {
+  var datLinkInputBox = document.querySelector('.dat-link-input')
+  var datLinkInputValue = datLinkInputBox.value
+
+  if (!isDatUrl(datLinkInputValue)) {
+    alert('Please try again with correct dat url.')
+    return
+  }
+
+  if (datlink == datLinkInputValue) {
+    return
+  }
+
+  datlink = datLinkInputValue
+
+  console.log('setting up src now')
+  vid.src = datlink
+})
+
